0226b3f273f99e8c22b2521aefe69592d53d43a4
Mateusz Szulc <mszulc@csod.com>
2020-11-19 01:29:30 +0100

    Jira remove call global

--- src/Jira.groovy
+++ src/Jira.groovy
@@ -10,7 +10,6 @@ class Jira implements Serializable {
     Jira(steps, JIRA_CREDENTIALS) {
         this.steps = steps;
         this.JIRA_CREDENTIALS = JIRA_CREDENTIALS;
-        log.info("dupa");
     }
 
     def post(jsonObject, endpoint) {
