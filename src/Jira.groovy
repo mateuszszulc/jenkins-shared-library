b16a1d3afc1d92fe827e12b86f7f566915bc725e
Mateusz Szulc <mszulc@csod.com>
2020-11-19 01:22:48 +0100

    Rearanged code - Jira -> src

---
+++ src/Jira.groovy
@@ -0,0 +1,21 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
+class Jira implements Serializable {
+    def JIRA_SERVER = "https://jira.lumesse.top"
+
+    def steps
+    def JIRA_CREDENTIALS
+
+    Jira(steps, JIRA_CREDENTIALS) {
+        this.steps = steps;
+        this.JIRA_CREDENTIALS = JIRA_CREDENTIALS;
+    }
+
+    def post(jsonObject, endpoint) {
+        def json = JsonOutput.toJson(jsonObject)
+        steps.echo "Hello world $JIRA_SERVER"
+        return new JsonSlurper().parseText('{"hello" : "world"}')
+    }
+}
+
--- vars/log.groovy
+++ vars/log.groovy
@@ -1,33 +1,11 @@
-package org.foo
+@groovy.transform.Field
+def CREATE_RM_ENDPOINT = "/rest/api/2/issue";
 
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-class Jira implements Serializable {
-    def JIRA_SERVER = "https://jira.lumesse.top"
-
-    def steps
-    def JIRA_CREDENTIALS
-
-    Jira(steps, JIRA_CREDENTIALS) {
-        this.steps = steps;
-        this.JIRA_CREDENTIALS = JIRA_CREDENTIALS;
-    }
-
-    def post(jsonObject, endpoint) {
-        def json = JsonOutput.toJson(jsonObject)
-        steps.echo "Hello world $JIRA_SERVER"
-        return new JsonSlurper().parseText('{"hello" : "world"}')
-    }
+def info(message) {
+    echo "INFO: ${message} ${CREATE_RM_ENDPOINT}"
 }
 
-//@groovy.transform.Field
-//def CREATE_RM_ENDPOINT = "/rest/api/2/issue";
-//
-//def info(message) {
-//    echo "INFO: ${message} ${CREATE_RM_ENDPOINT}"
-//}
-//
-//def warning(message) {
-//    echo "WARNING: ${message}"
-//}
-//
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
