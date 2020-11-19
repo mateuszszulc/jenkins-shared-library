3c9bcc3b1d0504e997da89efe9f4edb3a0ac9ec2
Mateusz Szulc <mszulc@csod.com>
2020-11-19 01:12:20 +0100

    Added Jira stubs

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,6 @@
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+
 @groovy.transform.Field
 def CREATE_RM_ENDPOINT = "/rest/api/2/issue";
 
@@ -7,4 +10,22 @@ def info(message) {
 
 def warning(message) {
     echo "WARNING: ${message}"
+}
+
+class Jira implements Serializable {
+    def JIRA_SERVER = "https://jira.lumesse.top"
+
+    def steps
+    def JIRA_CREDENTIALS
+
+    Jira(steps, JIRA_CREDENTIALS) {
+        this.steps = steps;
+        this.JIRA_CREDENTIALS = JIRA_CREDENTIALS;
+    }
+
+    def post(jsonObject, endpoint) {
+        def json = JsonOutput.toJson(jsonObject)
+        steps.echo "Hello world $JIRA_SERVER"
+        return new JsonSlurper().parseText('{"hello" : "world"}')
+    }
 }
