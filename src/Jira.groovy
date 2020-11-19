6ff79c0aa70d1bf94d47caf1af20c71aa8edd911
Mateusz Szulc <mszulc@csod.com>
2020-11-19 16:25:41 +0100

    Await feature

--- src/Jira.groovy
+++ src/Jira.groovy
@@ -5,11 +5,31 @@ class Jira implements Serializable {
     def JIRA_SERVER = "https://jira.lumesse.top"
 
     def steps
-    def JIRA_CREDENTIALS
 
     Jira(steps, JIRA_CREDENTIALS) {
         this.steps = steps;
         this.JIRA_CREDENTIALS = JIRA_CREDENTIALS;
+    }
+
+    def await() {
+        def i = 0;
+        def retryDelay = 15;
+
+        steps.withCredentials([steps.usernameColonPassword(credentialsId: 'mateusz', variable: 'JIRA_CREDENTIALS')]) {
+
+            steps.retry(8) { // 2 minutes
+                steps.echo "Password injected is: $JIRA_CREDENTIALS"
+                steps.sleep(retryDelay);
+
+                if (i < 5) {
+                    i++
+                    steps.echo "Issue status ID: $i"
+                    steps.error("Wrong status $i");
+                } else {
+                    steps.echo "Ticket is READY for PROD deployment ($i)"
+                }
+            }
+        }
     }
 
     def test() {
